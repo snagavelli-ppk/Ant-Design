d94beef2c6af0d5eeadfd5fb86dff471a3e824d4
Shiva <snagavelli@ppkvirtueservices.com>
2023-12-19 15:28:10 +0530

    responsive,multiple select with search option for dropdowns

--- PPK/src/components/CompanyAdmin.tsx
+++ PPK/src/components/CompanyAdmin.tsx
@@ -2,7 +2,7 @@ import type { FormInstance } from "antd";
 import { Button, Form, Input, Space, Row, Col, Select } from "antd";
 const { Option } = Select;
 const { TextArea } = Input;
-import './CompanyForm.css';
+import "./CompanyForm.css";
 import { useState, useEffect } from "react";
 
 const SubmitButton = ({ form }: { form: FormInstance }) => {
@@ -37,7 +37,12 @@ const ComanyForm: React.FC = () => {
 
   const urlPrefixOptions = ["http://", "https://"];
   const urlSuffixOptions = [".com", ".in", ".jp", ".uk", ".us"];
-  const phoneNumberPrefixOptions = ["+1 (USA)", "+44 (UK)", "+81 (Japan)", '+91 (IND)'];
+  const phoneNumberPrefixOptions = [
+    "+1 (USA)",
+    "+44 (UK)",
+    "+81 (Japan)",
+    "+91 (IND)",
+  ];
 
   const onFinish = (values: any) => {
     console.log("Form Data:", values);
@@ -46,177 +51,196 @@ const ComanyForm: React.FC = () => {
 
   return (
     <div>
-    <Form
-      form={form}
-      name="validateOnly"
-      layout="vertical"
-      autoComplete="off"
-      onFinish={onFinish}
-      style={{ width: "800px"}}
-    >
-      <h1 >Company Form</h1>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="company name"
-            label="Company Name"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="company url"
-            label="Company URL"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[
-              { required: true, message: "Please enter the company URL" },
-              {
-                message: "Please enter a valid URL",
-              },
-            ]}
-          >
-            <Input
-              addonBefore={
-                <Select defaultValue="http://">
-                  {urlPrefixOptions.map((option, index) => (
-                    <Option key={index} value={option}>
-                      {option}
-                    </Option>
-                  ))}
-                </Select>
-              }
-              addonAfter={
-                <Select defaultValue=".com">
-                  {urlSuffixOptions.map((option, index) => (
-                    <Option key={index} value={option}>
-                      {option}
-                    </Option>
-                  ))}
-                </Select>
-              }
-              style={{ width: "100%" }}
-            />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="tutorials"
-            label="Tutorials"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <Select placeholder="Select a tutorial">
-              {tutorialsOptions.map((option, index) => (
-                <Option key={index} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="simulations"
-            label="Simulations"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <Select placeholder="Select a simulation">
-              {simulationsOptions.map((option, index) => (
-                <Option key={index} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="primaryphonenumber"
-            label="Primary Phone Number"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[
-              { required: true, message: "Please enter your phone number", max: 10, min: 10 },
-              {
-                pattern: /^\d+$/,
-                message: "Please enter only numbers",
-              },
-            ]}
-          >
-            <Input
-              addonBefore={
-                <Select defaultValue={phoneNumberPrefixOptions[0]}>
-                  {phoneNumberPrefixOptions.map((option, index) => (
-                    <Option key={index} value={option}>
-                      {option}
-                    </Option>
-                  ))}
-                </Select>
-              }
-              style={{ width: "100%" }}
-            />
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="adminlinkextension"
-            label="Admin Link Extension"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="companyadmin"
-            label="Company Admin"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <Select placeholder="Select a company admin">
-              {companyAdminOptions.map((option, index) => (
-                <Option key={index} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="companyoptions"
-            label="Company Options"
-            hasFeedback
-            validateDebounce={1000}
-            rules={[{ required: true }]}
-          >
-            <TextArea rows={4} placeholder="Enter company options" />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Form.Item>
-        <Space style={{ justifyContent: "center" }}>
-          <SubmitButton form={form} />
-        </Space>
-      </Form.Item>
-    </Form>
+      <Form
+        form={form}
+        name="validateOnly"
+        layout="vertical"
+        autoComplete="off"
+        onFinish={onFinish}
+      >
+        <h1>Company Form</h1>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="company name"
+              label="Company Name"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="company url"
+              label="Company URL"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[
+                { required: true, message: "Please enter the company URL" },
+                {
+                  message: "Please enter a valid URL",
+                },
+              ]}
+            >
+              <Input
+                addonBefore={
+                  <Select defaultValue="http://">
+                    {urlPrefixOptions.map((option, index) => (
+                      <Option key={index} value={option}>
+                        {option}
+                      </Option>
+                    ))}
+                  </Select>
+                }
+                addonAfter={
+                  <Select defaultValue=".com">
+                    {urlSuffixOptions.map((option, index) => (
+                      <Option key={index} value={option}>
+                        {option}
+                      </Option>
+                    ))}
+                  </Select>
+                }
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="tutorials"
+              label="Tutorials"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <Select
+                mode="multiple"
+                showSearch
+                optionFilterProp="label"
+                placeholder="Select a tutorial"
+              >
+                {tutorialsOptions.map((option, index) => (
+                  <Option key={index} value={option} label={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="simulations"
+              label="Simulations"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <Select
+                placeholder="Select a simulation"
+                mode="multiple"
+                showSearch
+                optionFilterProp="label"
+              >
+                {simulationsOptions.map((option, index) => (
+                  <Option key={index} value={option} label={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="primaryphonenumber"
+              label="Primary Phone Number"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your phone number",
+                  max: 10,
+                  min: 10,
+                },
+                {
+                  pattern: /^\d+$/,
+                  message: "Please enter only numbers",
+                },
+              ]}
+            >
+              <Input
+                addonBefore={
+                  <Select defaultValue={phoneNumberPrefixOptions[0]}>
+                    {phoneNumberPrefixOptions.map((option, index) => (
+                      <Option key={index} value={option}>
+                        {option}
+                      </Option>
+                    ))}
+                  </Select>
+                }
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="adminlinkextension"
+              label="Admin Link Extension"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="companyadmin"
+              label="Company Admin"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <Select
+                mode="multiple"
+                placeholder="Select a company admin"
+                showSearch
+                optionFilterProp="label"
+              >
+                {companyAdminOptions.map((option, index) => (
+                  <Option key={index} value={option} label={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="companyoptions"
+              label="Company Options"
+              hasFeedback
+              validateDebounce={1000}
+              rules={[{ required: true }]}
+            >
+              <TextArea rows={4} placeholder="Enter company options" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item>
+          <Space style={{ justifyContent: "center" }}>
+            <SubmitButton form={form} />
+          </Space>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
